065504411a5739676f531beb3230d186cf069d91
Matthew Millett <millett@stanford.edu>
2018-11-03 17:46:51 -0700

    redux init ⚛

--- telypath/src/App.js
+++ telypath/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './redux/action'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const simpleAction = (event) => {
+     this.props.simpleAction();
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +16,14 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <div>
+            <button onClick={simpleAction}>Test redux action</button>
+            <pre>
+               {
+                JSON.stringify(this.props)
+               }
+              </pre>
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -25,4 +38,13 @@ class App extends Component {
   }
 }
 
-export default App;
+
+const mapStateToProps = state => ({
+ ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+ simpleAction: () => dispatch(simpleAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
